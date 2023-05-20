92b0ce77fefd0a82364100243c0698c7d67a17e3
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-05-20 13:34:52 +0530

    added css

--- TO-DO-List/src/App.js
+++ TO-DO-List/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <div className="container">
+      <div className="top">
+        <input type="text" className="text"/>
+        
+      </div>
+     </div>
     </div>
   );
 }
