2f952677c911bb2a61c71f2d6d8d281fc7ee5878
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-05-20 15:56:00 +0530

    first commit

---
+++ birthday-remainder/src/List.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const List = ({ people }) => {
+  return (
+    <>
+        <ul>
+        {people.map((person)=>{
+            const {id,name,age,image} = person;
+            return (
+                <li key={id} className='person'>
+                    <img src={image} alt={name} />
+                    <div>
+                        <h4>{name}</h4>
+                        <p>{age} years</p>
+                    </div>
+                </li>
+            )
+        })}
+        </ul>
+    </>
+  );
+};
+
+export default List;
