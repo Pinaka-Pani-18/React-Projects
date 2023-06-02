0c3a67c14c104483ca2e6da5c615b14f9cf19cdd
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-06-03 02:04:29 +0530

    first commit

---
+++ brand-page/src/App.jsx
@@ -0,0 +1,14 @@
+import './app.css'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+const App = () =>{
+  return(
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
+  );
+};
+
+export default App;
---
+++ brand-page/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () =>{
+
+    return(
+        <nav className="container">
+            <div className="logo">
+                <img src="../images/brand-logo.png" alt="" />
+            </div>
+            <ul className="menu">
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Collection</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+            <div className="buttons">
+                <button className="loginBtn">Login</button>
+                <button className="signUpBtn">Sign Up</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
