bb8c602497cd3d8906bd125a94856f31b16b7b1d
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-07-16 11:45:54 +0530

    added colors

--- crypto-dashboard/src/theme/index.js
+++ crypto-dashboard/src/theme/index.js
@@ -1,9 +1,30 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
+  colors: {
+    p: {
+      purple: "#5700d7",
+      black: "#171717",
+    },
+    gray: {
+      5: "#f3f3f7",
+      10: "#eeeef4",
+      20: "#d8dde2",
+      40: "#babac4",
+      60: "#797e82",
+      80: "#535d66",
+    },
+  },
   fonts: {
     heading: `Ubuntu`,
     body: `Ubuntu`,
+  },
+  styles: {
+    global: {
+      "html, body": {
+        bg: "#f3f3f7",
+      },
+    },
   },
 });
 
