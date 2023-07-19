ba194fa8c607ce1e802b575d312c0b31a3d86102
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-07-19 17:01:02 +0530

    completed the portfolio and price sections

--- crypto-dashboard/src/pages/Dashboard/Dashboard.jsx
+++ crypto-dashboard/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,27 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import DashBoardLayout from "../../components/DashBoardLayout";
+import PortfolioSection from "./components/PortfolioSection";
+import PriceSection from "./components/PriceSection";
 
 const Dashboard = () => {
-  return <DashBoardLayout title={"DashBoard"}>How are you ?</DashBoardLayout>;
+  return (
+    <DashBoardLayout title={"DashBoard"}>
+      <Grid
+        gridTemplateColumns={{
+          base: "repeat(1, 1fr)",
+          md: "repeat(2, 1fr)",
+        }}
+        gap={"6"}
+      >
+        <GridItem colSpan={2}>
+          <PortfolioSection />
+        </GridItem>
+        <GridItem>
+          <PriceSection colSpan={1} />
+        </GridItem>
+      </Grid>
+    </DashBoardLayout>
+  );
 };
 
 export default Dashboard;
