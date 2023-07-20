c26e510d1fb48f75117364e62fb85bba43668597
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-07-20 11:36:40 +0530

    Transaction Page completed

--- crypto-dashboard/src/components/SideNav.jsx
+++ crypto-dashboard/src/components/SideNav.jsx
@@ -2,6 +2,7 @@ import { Box, HStack, Heading, Icon, Stack, Text } from "@chakra-ui/react";
 import { RxDashboard } from "react-icons/rx";
 import { BsArrowDownUp } from "react-icons/bs";
 import { BiSupport } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const SideNav = () => {
   const navLinks = [
@@ -37,38 +38,41 @@ const SideNav = () => {
         <Box mt={"6"} mx={"3"}>
           {navLinks.map((nav) => {
             return (
-              <HStack
-                borderRadius={"10px"}
-                color={"#797E82"}
-                px={"3"}
-                py={"4"}
-                _hover={{ bg: "#f3f3f7", color: "#171717" }}
-                key={nav.text}
-                cursor={"pointer"}
-              >
-                <Icon as={nav.icon} />
-                <Text fontWeight="medium" fontSize={"14px"}>
-                  {nav.text}
-                </Text>
-              </HStack>
+              <Link key={nav.text} to={nav.link}>
+                <HStack
+                  borderRadius={"10px"}
+                  color={"#797E82"}
+                  px={"3"}
+                  py={"4"}
+                  _hover={{ bg: "#f3f3f7", color: "#171717" }}
+                  cursor={"pointer"}
+                >
+                  <Icon as={nav.icon} />
+                  <Text fontWeight="medium" fontSize={"14px"}>
+                    {nav.text}
+                  </Text>
+                </HStack>
+              </Link>
             );
           })}
         </Box>
       </Box>
       <Box mb={"6"} mx={"3"}>
-        <HStack
-          borderRadius={"10px"}
-          color={"#797E82"}
-          px={"3"}
-          py={"4"}
-          _hover={{ bg: "#f3f3f7", color: "#171717" }}
-          cursor={"pointer"}
-        >
-          <Icon as={BiSupport} />
-          <Text fontWeight="medium" fontSize={"14px"}>
-            Support
-          </Text>
-        </HStack>
+        <Link to={"/support"}>
+          <HStack
+            borderRadius={"10px"}
+            color={"#797E82"}
+            px={"3"}
+            py={"4"}
+            _hover={{ bg: "#f3f3f7", color: "#171717" }}
+            cursor={"pointer"}
+          >
+            <Icon as={BiSupport} />
+            <Text fontWeight="medium" fontSize={"14px"}>
+              Support
+            </Text>
+          </HStack>
+        </Link>
       </Box>
     </Stack>
   );
