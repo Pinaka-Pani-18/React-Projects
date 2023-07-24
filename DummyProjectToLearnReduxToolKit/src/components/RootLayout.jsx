727980f5b40bebabdf092380dbea7b342d0e303e
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-07-24 19:51:27 +0530

    React router dom setup completed

---
+++ DummyProjectToLearnReduxToolKit/src/components/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+const RootLayout = () => {
+  return (
+    <>
+      <h1>Navigation</h1>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default RootLayout;
