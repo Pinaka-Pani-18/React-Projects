694f8ac00a5bc075ad16fee5eae96415f3f005f6
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-07-26 00:28:02 +0530

    navbar, react router setup completed

---
+++ useContextANDuseReducerMiniProject/src/components/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>I am home page</div>;
+};
+
+export default Home;
---
+++ useContextANDuseReducerMiniProject/src/context/Context.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+
+const Cart = createContext();
+
+const Context = ({ children }) => {
+  return <Cart.Provider>{children}</Cart.Provider>;
+};
+
+export default Context;
